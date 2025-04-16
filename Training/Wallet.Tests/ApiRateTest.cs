3e3f14cad3556d498212bcedda9ccdf1e3574042
Jérémy Sorant <jsorant@gmail.com>
2025-04-16 16:07:00 +0200

    feat: handle different types of currency in api

--- Training/Wallet.Tests/ApiRateTest.cs
+++ Training/Wallet.Tests/ApiRateTest.cs
@@ -2,10 +2,8 @@ namespace Wallet.Tests;
 
 public class ApiRateTest
 {
-    // private static readonly HttpClient Client = new();
-
     [Fact]
-    private void ShouldGetEuroToDollarRate2()
+    private void ShouldGetEuroToDollarRate()
     {
         var rateProvider = new RestRateProvider();
 
@@ -14,39 +12,13 @@ public class ApiRateTest
         Assert.NotEqual(0, rate);
     }
 
-    // [Fact]
-    // private async Task ShouldGetEuroToDollarRate()
-    // {
-    //     Client.BaseAddress = new Uri("https://api.frankfurter.dev/");
-    //     Client.DefaultRequestHeaders.Accept.Clear();
-    //     Client.DefaultRequestHeaders.Accept.Add(
-    //         new MediaTypeWithQualityHeaderValue("application/json"));
-    //
-    //     var response = await Client.GetAsync("v1/latest?base=EUR&symbols=USD");
-    //
-    //     Assert.True(response.IsSuccessStatusCode);
-    //
-    //     var rateResponse = await response.Content.ReadFromJsonAsync<RateResponse>();
-    //     Assert.NotNull(rateResponse);
-    //     Assert.IsType<double>(rateResponse.Rates.USD);
-    //     Assert.True(rateResponse.Rates.USD > 1);
-    // }
-    //
-    // [Fact]
-    // private async Task ShouldGetDollarToEuroRate()
-    // {
-    //     Client.BaseAddress = new Uri("https://api.frankfurter.dev/");
-    //     Client.DefaultRequestHeaders.Accept.Clear();
-    //     Client.DefaultRequestHeaders.Accept.Add(
-    //         new MediaTypeWithQualityHeaderValue("application/json"));
-    //
-    //     var response = await Client.GetAsync("v1/latest?base=USD&symbols=EUR");
-    //
-    //     Assert.True(response.IsSuccessStatusCode);
-    //
-    //     var rateResponse = await response.Content.ReadFromJsonAsync<RateResponse>();
-    //     Assert.NotNull(rateResponse);
-    //     Assert.IsType<double>(rateResponse.Rates.EUR);
-    //     Assert.True(rateResponse.Rates.EUR > 1);
-    // }
+    [Fact]
+    private void ShouldGetEuroToEuroRate()
+    {
+        var rateProvider = new RestRateProvider();
+
+        var rate = rateProvider.Rate(StockType.Euro, Currency.Euro);
+
+        Assert.Equal(1, rate);
+    }
 }
--- Training/Wallet.Tests/RESTRateProvider.cs
+++ Training/Wallet.Tests/RESTRateProvider.cs
@@ -5,20 +5,51 @@ namespace Wallet.Tests;
 
 public class RestRateProvider : IRateProvider
 {
+    private static readonly Dictionary<StockType, string> StockTypeToApiLabels = new()
+    {
+        {
+            StockType.Euro, "EUR"
+        },
+        {
+            StockType.Dollar, "USD"
+        }
+    };
+
+    private static readonly Dictionary<Currency, string> CurrencyToApiLabels = new()
+    {
+        {
+            Currency.Euro, "EUR"
+        },
+        {
+            Currency.Dollar, "USD"
+        }
+    };
+
     private static readonly HttpClient Client = new();
+
 
     public double Rate(StockType from, Currency to)
     {
+        if (StockTypeToApiLabels[from] == CurrencyToApiLabels[to]) return 1;
         Client.BaseAddress = new Uri("https://api.frankfurter.dev/");
         Client.DefaultRequestHeaders.Accept.Clear();
         Client.DefaultRequestHeaders.Accept.Add(
             new MediaTypeWithQualityHeaderValue("application/json"));
 
-        var response = Client.GetAsync("v1/latest?base=EUR&symbols=USD");
+        var path = $"v1/latest?base={StockTypeToApiLabels[from]}&symbols={CurrencyToApiLabels[to]}";
+
+        var response = Client.GetAsync(path);
 
         var rateResponse = response.Result.Content.ReadFromJsonAsync<RateResponse>().Result;
 
-        return rateResponse.Rates.USD;
+        if (rateResponse == null)
+            throw new ApplicationException("Unable to get rate");
+        return to switch
+        {
+            Currency.Dollar => rateResponse.Rates.USD,
+            Currency.Euro => rateResponse.Rates.EUR,
+            _ => throw new ArgumentOutOfRangeException(nameof(to), to, null)
+        };
     }
 }
 
